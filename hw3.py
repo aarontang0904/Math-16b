8d5c5a167c674558985cda5b3cac4290494958c9
aarontang0904 <tangyuang01@gmail.com>
2024-01-31 23:03:53 -0500

    first complete edition

--- hw3.py
+++ hw3.py
@@ -2,6 +2,8 @@
 Use this space to copy and paste functions you wrote in 
 the last class. For example:
 '''
+def norm(v):
+  return sum([i**2 for i in v])**0.5
 
 def dot(v1: list, v2: list) -> float:
   '''
@@ -16,10 +18,10 @@ def dot(v1: list, v2: list) -> float:
 
   Returns the dot product of v1 and v2
   '''
+  return sum([i * j for i,j in zip(v1,v2)]) if len(v1) == len(v2) else None
 
-# solution starts here:
-  return sum([i * j for i,j in zip(v1,v2,strict=True)]) if len(v1) == len(v2) else None
-
+# v1, v2 are linearly dependentif there is some a, b spans R such that a*v1+b*v2 = 0 for nonzero a, b
+# v1 and v2 are colinear v1 is nonzero, there exists a scalar multiple of v1 that is equal to v2
 def is_scalar_multiple(v1, v2):
   '''
   Parameters
@@ -29,11 +31,18 @@ def is_scalar_multiple(v1, v2):
 
   Returns
   -------
-  True if v1 and v2 are lienarly independent, and False otherwise
+  True if v1 and v2 are linearly dependent, and False otherwise
   '''
-  
-  
-
+  # Check if either of them is the zero vector
+  if norm(v1) == 0 or norm(v2) == 0:
+    return True 
+  # Find the first nonzero entry of v2
+  for i,value in enumerate(v2):
+    if value != 0:
+      c = v1[i]/v2[i]
+      break
+  # Check c*v2 = v1
+  return [i*c for i in v2] == v1
 
 def count_vectors(vectors):
   '''
@@ -48,10 +57,19 @@ def count_vectors(vectors):
   are the number of occurances of each vector. 
 
   For example:
-  count_vectors([(1,0), (1,0), (2,3), (0,0), (0)])
-    = {(1,0) : 2, (2,3) : 1, (0,0): 1, (0): 1}
+  count_vectors([(1,0), (1,0), (2,3), (0,0), (0,)])
+    = {(1,0) : 2, (2,3) : 1, (0,0): 1, (0,): 1}
   '''
-  pass
+  # First check if the list is empty
+  if not vectors:
+    return {}
+  output = {} # Initialize the output dictionary
+  for vector in vectors:
+    if vector in output:
+      output[vector] += 1
+    else:
+      output[vector] = 1
+  return output
 
 
 def reverse_dictionary(d):
@@ -69,7 +87,14 @@ def reverse_dictionary(d):
   reverse_dictionary({1 : a, 2: b, 3: b}) 
     ={a : 1, b: [2,3]}
   '''
-  pass
+  output = {}
+  for key, value in d.items():
+    if value in output:
+      output[value].append(key)
+    else:
+      output[value] = [key]
+  return output
+
 
 def normalize(v):
   '''
@@ -83,7 +108,7 @@ def normalize(v):
 
   Returns v/|v|
   '''
-  pass
+  return [i/norm(v) for i in v]
 
 def orthogonal_projection(v, w):
   '''
@@ -100,6 +125,16 @@ def orthogonal_projection(v, w):
     2. x is parallel to w
     3. y is perpendicular to w
   '''
-  pass
+  '''
+  Formulas: 
+    x = aw
+    y = v - aw
+    y*w = 0 = v*w - a*||w||^2
+    a = v*w/||w||^2
+  '''
+  alpha = dot(v, w)/(norm(w)^2)
+  x = [alpha * i for i in w]
+  y = [i - j for i, j in zip(v,x)]
+  return x, y
 
 
